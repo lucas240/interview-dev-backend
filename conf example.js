6837b63ceb1d6e30b72c49a9602b732403fa5a7a
lucas <lucasleproust@gmail.com>
2021-02-22 16:03:16 +0100

    delete conf example

--- conf example.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "", // address of the server
-  user: "", // username
-  password: "!",
-  database: "",
-});
-module.exports = connection;
