3a86f25c0a9de105da75eea77722e040b6696457
lucas <lucasleproust@gmail.com>
2021-02-22 02:48:34 +0100

    create conf example for reference, install cors for frontend request, change members post route

--- conf example.js
+++ conf example.js
@@ -1,8 +1,8 @@
-const  mysql = require('mysql');
-const  connection = mysql.createConnection({
-host :  '', // address of the server
-user :  '', // username
-password :  '!',
-database :  '',
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "", // address of the server
+  user: "", // username
+  password: "!",
+  database: "",
 });
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
-const connection = require('./conf');
+const port = 8000;
+const connection = require("./conf");
+const cors = require("cors");
 
-app.get('/api/members', (req, res) => {
-    connection.query('SELECT * from member', (err, results) => {
-    if (err) {
-        res.status(500).send('Error retrieving data');
-    } else {
-        res.json(results);
-    }});
-});  
+app.use(cors());
+app.use(express.json());
 
-app.post('/api/members', (req, res) => {
-    const formData = req.body;
-    connection.query('INSERT INTO member SET ?', formData, (err, results) => {
+app.get("/members", (req, res) => {
+  connection.query("SELECT * from member", (err, results) => {
     if (err) {
-        console.log(err);
-        res.status(500).send("Error saving an member");
+      res.status(500).send("Error retrieving data");
     } else {
-        res.sendStatus(200);
+      res.json(results);
     }
-    });
+  });
 });
 
-// app.delete('/api/movie/:id', (req, res) => {
-//     const idMovie = req.params.id;
-//     connection.query('DELETE FROM movie WHERE id = ?', [idMovie], err => {
-//         if (err) {
-//             console.log(err);
-//             res.status(500).send("Error deleting a movie");
-//         } else {
-//             res.sendStatus(200);
-//         }
-//     });
-    
-// });
+app.post("/members", (req, res) => {
+  const formData = req.body;
+  connection.query("INSERT INTO member SET ?", formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error saving an member");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
 
 app.listen(port, (err) => {
-    if (err) {
-        throw new Error('Something bad happened...');
-    }
-    console.log(`Server is listening on ${port}`);
+  if (err) {
+    throw new Error("Something bad happened...");
+  }
+  console.log(`Server is listening on ${port}`);
 });
